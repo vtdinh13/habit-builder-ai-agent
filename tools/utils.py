74749f4c6f79ee45cfbd134fd41f27519f34ac0a
Vancesca Dinh <dinh.vancesca@gmail.com>
2025-12-01 12:37:10 +0100

    edit: organizing

---
+++ tools/utils.py
@@ -0,0 +1,55 @@
+from pydantic_ai.messages import FunctionToolCallEvent
+from dataclasses import dataclass
+
+class NamedCallback:
+
+    def __init__(self, agent):
+        self.agent_name = agent.name
+
+    async def print_function_calls(self, ctx, event):
+        # Detect nested streams
+        if hasattr(event, "__aiter__"):
+            async for sub in event:
+                await self.print_function_calls(ctx, sub)
+            return
+
+        if isinstance(event, FunctionToolCallEvent):
+            tool_name = event.part.tool_name
+            args = event.part.args
+            print(f"TOOL CALL ({self.agent_name}): {tool_name}({args})")
+
+    async def __call__(self, ctx, event):
+        return await self.print_function_calls(ctx, event)
+
+@dataclass
+class AgentConfig:
+    model:str = "openai:gpt-4o-mini"
+
+
+preferred_sites = [
+    "brainfacts",
+    "nimh",
+    "nih",
+    "alleninstitute",
+    "mit",
+    "stanford",
+    "acsm",
+    "nsca",
+    "acefitness",
+    "exerciseismedicine",
+    "bjsm",
+    "apa",
+    "motivationscience",
+    "berkeley",
+    "mayoclinic",
+    "clevelandclinic",
+    "harvard",
+    "hopkinsmedicine",
+    "cdc",
+    "mpg",
+    "yale",
+    "scientificamerican",
+    "psychologytoday",
+    "nature",
+    "science",
+]
