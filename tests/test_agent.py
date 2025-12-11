8fd2eba2db3350277b618c4de73f281b05aa745f
Vancesca Dinh <dinh.vancesca@gmail.com>
2025-12-11 17:13:20 +0100

    edit: test reference per section

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -7,10 +7,10 @@ def test_search_references():
     result = main.run_agent_sync(user_prompt=user_prompt)
 
     response: SearchResultResponse = result.output
-    print(response.format_response())
+    # print(response.format_response())
 
-    assert len(response.references) >=1, f"Expecting at least one reference, got {len(response.references)}"
-    # assert all(section.references for section in response.sections), "Expecting at least one reference in each section."
+    # assert len(response.references) >=1, f"Expecting at least one reference, got {len(response.references)}"
+    assert all(section.references for section in response.sections), "Expecting at least one reference in each section."
 
 def test_get_page_content_no_more_than_3():
     user_prompt = "What is the latest research on coffee and Alzheimer's?"
@@ -19,5 +19,5 @@ def test_get_page_content_no_more_than_3():
     tool_calls = get_tool_calls(result)
     get_page_content_calls = [t for t in tool_calls if t.name == "get_page_content"]
 
-    assert len(get_page_content_calls) <= 3, f"Expecting no more than 3 tool alls, got {len(get_page_content_calls)}"
+    assert len(get_page_content_calls) <= 3, f"Expecting no more than 3 get_page_content tool calls, got {len(get_page_content_calls)}"
 
